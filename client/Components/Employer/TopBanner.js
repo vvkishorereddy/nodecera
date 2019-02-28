5ba43fff002b5fb1d70869f01b82b5e49839e3f9
kishore Reddy <vvkishorereddy@gmail.com>
2019-03-01 00:51:48 +0530

    changes

--- client/Components/Employer/TopBanner.js
+++ client/Components/Employer/TopBanner.js
@@ -1,86 +1,75 @@
 import React, { Component } from "react";
+import withContext from "../../hoc/ContextConsumer";
 
-export default class TopBanner extends Component {
+class TopBanner extends Component {
   render() {
+    const {
+      company_name,
+      company_location,
+      company_phone,
+      company_email,
+      company_website,
+      company_logo,
+      socialLinks
+    } = this.props.context.companyInfo;
+
     return (
       <div className="tr-breadcrumb bg-image section-before">
         <div className="container">
           <div className="breadcrumb-info text-center">
             <div className="breadcrumb-logo">
-              <img
-                src="images/others/company-logo.png"
-                alt="Logo"
-                className="img-fluid"
-              />
+              <img src={company_logo} alt="Logo" className="img-fluid" />
             </div>
             <div className="page-title">
-              <h1>Google inc</h1>
+              <h1>{company_name}</h1>
             </div>
             <ul className="job-meta tr-list list-inline">
               <li>
                 <i className="fa fa-map-marker" aria-hidden="true" />
-                San Francisco, CA, US
+                {company_location}
               </li>
               <li>
                 <i className="fa fa-phone" aria-hidden="true" />
-                +0123 456 789
+                {company_phone}
               </li>
               <li>
                 <i className="fa fa-envelope" aria-hidden="true" />
-                <a href="employer-profile.html#">
-                  <span
-                    className="__cf_email__"
-                    data-cfemail="394a4c4949564b4d795e56565e555c175a5654"
-                  >
-                    [email&nbsp;protected]
-                  </span>
-                </a>
+                {company_email}
               </li>
               <li>
                 <i className="fa fa-briefcase" aria-hidden="true" />
                 IT Industry
               </li>
-              <li>
-                <i className="fa fa-black-tie" aria-hidden="true" />
-                Company Size: 2K
-              </li>
             </ul>
             <ul className="breadcrumb-social social-icon-bg  tr-list">
               <li>
-                <a href="employer-profile.html#">
+                <a href={socialLinks.facebook} target="_blank">
                   <i className="fa fa-facebook" />
                   <span>Facebook</span>
                 </a>
               </li>
               <li>
-                <a href="employer-profile.html#">
+                <a href={socialLinks.twitter} target="_blank">
                   <i className="fa fa-twitter" /> <span>Twitter</span>{" "}
                 </a>
               </li>
               <li>
-                <a href="employer-profile.html#">
-                  <i className="fa fa-google-plus" /> <span>Google Plus</span>{" "}
+                <a href={socialLinks.instagram} target="_blank">
+                  <i className="fa fa-instagram" /> <span>Instagram</span>{" "}
                 </a>
               </li>
               <li>
-                <a href="employer-profile.html#">
+                <a href={socialLinks.linkedin} target="_blank">
                   <i className="fa fa-linkedin" />
                   <span>Linkedin</span>{" "}
                 </a>
               </li>
               <li>
-                <a href="employer-profile.html#">
-                  <i className="fa fa-dribbble" /> <span>Dribbble</span>
-                </a>
-              </li>
-              <li>
-                <a href="employer-profile.html#">
-                  <i className="fa fa-behance" /> <span>Behance</span>
-                </a>
-              </li>
-              <li>
-                <a href="employer-profile.html#">
-                  <i className="fa fa-globe" /> <span>Website</span>{" "}
+                <a
+                  href={`http://${company_website.replace("http://", "")}`}
+                  target="_blank"
+                >
+                  <i className="fa fa-globe" /> <span>Website</span>
                 </a>
               </li>
             </ul>
@@ -91,3 +80,5 @@ export default class TopBanner extends Component {
     );
   }
 }
+
+export default withContext(TopBanner);
